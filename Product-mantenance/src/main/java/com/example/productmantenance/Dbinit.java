eaa8400a3b943ee9a70ae29a578e25b8198a1c37
Khanh Tran <104545255+khanhtran219@users.noreply.github.com>
2023-11-02 19:02:31 +0700

    commit

--- Product-mantenance/src/main/java/com/example/productmantenance/Dbinit.java
+++ Product-mantenance/src/main/java/com/example/productmantenance/Dbinit.java
@@ -19,11 +19,12 @@ public class Dbinit implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.productRepository.deleteAll();
-        Product product1 = new Product("8601","86 (the band) - True Life Songs and Pictures",14.95);
+        Product product1 = new Product("khanh","86 (the band) - True Life Songs and Pictures",14.95);
         Product product2 = new Product("pf01","Paddlefoot - The first CD",12.95);
         Product product3 = new Product("pf02","Paddlefoot - The Paddlefoot - The first CDsecond CD",14.95);
         Product product4 = new Product("jr01","Joe Rut - Genuine Wood Grained Finish",14.95);
         List<Product> products = Arrays.asList(product1, product2, product3);
         productRepository.saveAll(products);
+    // hello
     }
 }
